012f9f6671d65e73c41060d7eb16a243bd0038b1
Andrew Talle <talle.andrew@gmail.com>
2020-12-13 21:15:06 -0800

    first commit

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const BlogPage = () => {
+  return (
+    <div>
+      <h1>BLog</h1>
+      <p>Here will be the posts</p>
+    </div>
+  )
+}
+
+export default BlogPage
